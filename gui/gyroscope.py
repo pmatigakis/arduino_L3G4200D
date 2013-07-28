52c2d93ac77bea50cfdca17fcebeeaa16344e8d0
Panagiotis Matigakis <pmatigakis@gmail.com>
2013-07-28 18:16:14 +0300

    Added a better gui for the gyroscope

--- gui/gyroscope.py
+++ gui/gyroscope.py
@@ -4,8 +4,11 @@ import pygame
 SERIAL_PORT = "/dev/ttyACM0"
 DPS = 250.0
 MAX_DPS_VALUE = float(2**16)
+DT = 1.0 / 100.0
 
-screen_size = (320, 240)
+width = 640
+height = 480
+screen_size = (width, height)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
@@ -17,12 +20,10 @@ serial_port = serial.Serial(SERIAL_PORT)
 
 font = pygame.font.Font(None, 30)
 
-x_sum = 0.0
-y_sum = 0.0
-z_sum = 0.0
+x = 0.0
 
-n_samples = 20
-cnt = 0
+pos = [i for i in range(640)]
+x_data = [0.0 for i in range(640)]
 
 try:
     while True:
@@ -30,12 +31,11 @@ try:
         try:
             x_rot, y_rot, z_rot = line.strip().split("\t")
             x_rot = float(x_rot) * (DPS / MAX_DPS_VALUE)
-            y_rot = float(y_rot) * (DPS / MAX_DPS_VALUE)
-            z_rot = float(z_rot) * (DPS / MAX_DPS_VALUE)            
-            cnt += 1
-            x_sum += x_rot
-            y_sum += y_rot
-            z_sum += z_rot
+
+            x += x_rot * DT
+
+            x_data.append(x)
+            x_data.pop(0)
         except:
             print("Failed to parse data")
 
@@ -44,36 +44,13 @@ try:
                 serial_port.close()
                 exit()
 
-        if cnt == n_samples:
-            x_rot = x_sum / n_samples
-            y_rot = y_sum / n_samples
-            z_rot = z_sum / n_samples
-            x_sum = 0.0
-            y_sum = 0.0
-            z_sum = 0.0
-            cnt = 0
+        screen.fill(black)
+        
+        adjusted_data = [d + height/2 for d in x_data]
 
-            screen.fill(black)
+        pygame.draw.lines(screen, red, False, zip(pos, adjusted_data), 5)
 
-            x_rot_text = font.render("X dps: %f" % x_rot, 0, red)
-            x_rot_rect = x_rot_text.get_rect()
-            x_rot_rect.left = 50
-
-            y_rot_text = font.render("Y dps: %f" % y_rot, 0, red)
-            y_rot_rect = y_rot_text.get_rect()
-            y_rot_rect.top = 100
-            y_rot_rect.left = 50
-
-            z_rot_text = font.render("Z dps: %f" % z_rot, 0, red)
-            z_rot_rect = z_rot_text.get_rect()
-            z_rot_rect.top = 200
-            z_rot_rect.left = 50
-
-            screen.blit(x_rot_text, x_rot_rect)
-            screen.blit(y_rot_text, y_rot_rect)
-            screen.blit(z_rot_text, z_rot_rect)
-
-            pygame.display.flip()
+        pygame.display.flip()
 except KeyboardInterrupt:
     print("Shutting down")
 finally:
